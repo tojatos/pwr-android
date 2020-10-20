f74926a4d18d61b4183f9fa7af3dc8fc810b663e
tojatos <tojatos@gmail.com>
2020-10-20 11:17:12 +0200

    bmi format change added

--- pwr1/app/src/main/java/pl/krzysztofruczkowski/pwr1/MainActivity.kt
+++ pwr1/app/src/main/java/pl/krzysztofruczkowski/pwr1/MainActivity.kt
@@ -3,13 +3,16 @@ package pl.krzysztofruczkowski.pwr1
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import pl.krzysztofruczkowski.pwr1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var europeanFormat = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,11 +31,17 @@ class MainActivity : AppCompatActivity() {
         //TODO odt. stanu
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
     fun getBmiColor(bmi : Double) : Int {
         val colorString = when {
             bmi < 16 -> "#082E79"
             bmi < 16.99 -> "#4169E1"
-            bmi < 18.49 -> "##ACE1AF"
+            bmi < 18.49 -> "#ACE1AF"
             bmi < 24.99 -> "#CDEBA7"
             bmi < 29.99 -> "#FFFF99"
             bmi < 34.99 -> "#FDE456"
@@ -43,24 +52,47 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun count(view: View) {
+        updateBmi()
+    }
+
+    fun updateBmi() {
         binding.apply {
-            if (heightET.text.isBlank()) {
-                heightET.error = getString(R.string.height_is_empty)
-                return
-            }
+
+            var error = false
 
             if (massET.text.isBlank()) {
                 massET.error = getString(R.string.mass_is_empty)
-                return
+                error = true
             }
+
+            if (heightET.text.isBlank()) {
+                heightET.error = getString(R.string.height_is_empty)
+                error = true
+            }
+
+            if(error) return
 
             val mass = massET.text.toString().toDouble()
             val height = heightET.text.toString().toDouble()
 
-            val resultBmi = BmiForCmKg(mass, height).count()
+            val resultBmi = if (europeanFormat) BmiForCmKg(mass, height).count() else BmiForInLb(mass, height).count()
 
             bmiTV.text = resultBmi.toString()
             bmiTV.setTextColor(getBmiColor(resultBmi))
         }
+    }
+
+    fun changeBmiFormat(item: MenuItem) {
+        europeanFormat = !europeanFormat
+        binding.apply {
+            if(europeanFormat) {
+                heightTV.text = getString(R.string.height_cm)
+                massTV.text = getString(R.string.mass_kg)
+            } else {
+                heightTV.text = getString(R.string.height_in)
+                massTV.text = getString(R.string.mass_lb)
+            }
+        }
+        updateBmi()
     }
 }
