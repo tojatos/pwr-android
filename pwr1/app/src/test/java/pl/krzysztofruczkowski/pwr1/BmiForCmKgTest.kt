45bf6c948134b82bd4f7b40b6ab0f8e9c7a6560b
tojatos <tojatos@gmail.com>
2020-10-26 21:11:35 +0100

    Added kotest with bmi tests

---
+++ pwr1/app/src/test/java/pl/krzysztofruczkowski/pwr1/BmiForCmKgTest.kt
@@ -0,0 +1,16 @@
+package pl.krzysztofruczkowski.pwr1
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.doubles.plusOrMinus
+import io.kotest.matchers.shouldBe
+
+class BmiForCmKgTest : FunSpec({
+    test("Bmi should be calculated correctly") {
+        BmiForCmKg(68.0, 182.0).count() shouldBe(20.52 plusOrMinus 0.01)
+        BmiForCmKg(52.0, 163.0).count() shouldBe(19.57 plusOrMinus 0.01)
+    }
+
+    test("Zero height should return BMI equal to zero") {
+        BmiForCmKg(68.0, 0.0).count() shouldBe 0
+    }
+})
---
+++ pwr1/app/src/test/java/pl/krzysztofruczkowski/pwr1/BmiForInLbTest.kt
@@ -0,0 +1,16 @@
+package pl.krzysztofruczkowski.pwr1
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.doubles.plusOrMinus
+import io.kotest.matchers.shouldBe
+
+class BmiForInLbTest : FunSpec({
+    test("Bmi should be calculated correctly") {
+        BmiForInLb(68.0, 182.0).count() shouldBe(1.44 plusOrMinus 0.01)
+        BmiForInLb(52.0, 163.0).count() shouldBe(1.37 plusOrMinus 0.01)
+    }
+
+    test("Zero height should return BMI equal to zero") {
+        BmiForInLb(68.0, 0.0).count() shouldBe 0
+    }
+})
