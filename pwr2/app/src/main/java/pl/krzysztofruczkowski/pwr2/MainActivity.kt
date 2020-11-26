3670e96a0e0ad44a750578eda0ae89a4fbda449c
tojatos <tojatos@gmail.com>
2020-11-26 16:19:13 +0100

    added livedata

--- pwr2/app/src/main/java/pl/krzysztofruczkowski/pwr2/MainActivity.kt
+++ pwr2/app/src/main/java/pl/krzysztofruczkowski/pwr2/MainActivity.kt
@@ -2,8 +2,11 @@ package pl.krzysztofruczkowski.pwr2
 
 import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import pl.krzysztofruczkowski.pwr2.databinding.MainActivityBinding
@@ -15,17 +18,16 @@ class MainActivity : AppCompatActivity() {
         lateinit var app_resources: Resources
         lateinit var app_package_name: String
     }
+
     private lateinit var binding: MainActivityBinding
-    private val pokemons = arrayListOf(
-        Pokemon("Jolteon", "If it is angered or startled, the fur all over its body bristles like sharp needles that pierce foes. ", PokeCategory.Lightning),
-        Pokemon("Bulbasaur", "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.", PokeCategory.Seed),
-        Pokemon("Pikachu", "Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.", PokeCategory.Mouse),
-        Pokemon("Charizard", "It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames. ", PokeCategory.Flame),
-        Pokemon("Raichu", "Its long tail serves as a ground to protect itself from its own high-voltage power. ", PokeCategory.Mouse),
-        Pokemon("Flareon", "Once it has stored up enough heat, this Pokémon’s body temperature can reach up to 1,700 degrees Fahrenheit. ", PokeCategory.Flame),
-        Pokemon("Electrike", "It stores static electricity in its fur for discharging. It gives off sparks if a storm approaches. ", PokeCategory.Lightning),
-        Pokemon("Sunkern", "Sunkern tries to move as little as it possibly can. It does so because it tries to conserve all the nutrients it has stored in its body for its evolution. It will not eat a thing, subsisting only on morning dew. ", PokeCategory.Seed),
-    )
+    private lateinit var viewModel: MainViewModel
+
+    private val removeItemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+        override fun onMove( recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder) =
+            false
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) =
+            viewModel.onPokemonSwipe(viewHolder.adapterPosition)
+    })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,27 +36,15 @@ class MainActivity : AppCompatActivity() {
         app_package_name = packageName
 
         binding = MainActivityBinding.inflate(layoutInflater)
-        val adapter = PokemonsAdapter(pokemons)
-        binding.rvPokemons.adapter = adapter
-        val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-//                Toast.makeText(applicationContext, "on Move", Toast.LENGTH_SHORT).show()
-                return false
-            }
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-//                Toast.makeText(applicationContext, "on Swiped", Toast.LENGTH_SHORT).show()
-                val position = viewHolder.adapterPosition
-                pokemons.removeAt(position)
-                adapter.notifyDataSetChanged()
-            }
+        binding.rvPokemons.adapter = PokemonsAdapter(viewModel.pokemons.value ?: emptyList())
+
+        viewModel.pokemons.observe(this, {
+            binding.rvPokemons.adapter = PokemonsAdapter(it)
         })
-        itemTouchHelper.attachToRecyclerView(binding.rvPokemons)
 
+        removeItemTouchHelper.attachToRecyclerView(binding.rvPokemons)
         setContentView(binding.root)
     }
 }
---
+++ pwr2/app/src/main/java/pl/krzysztofruczkowski/pwr2/MainViewModel.kt
@@ -0,0 +1,33 @@
+package pl.krzysztofruczkowski.pwr2
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import pl.krzysztofruczkowski.pwr2.models.PokeCategory
+import pl.krzysztofruczkowski.pwr2.models.Pokemon
+
+class MainViewModel : ViewModel() {
+    private val _pokemons = MutableLiveData<List<Pokemon>>()
+    val pokemons: LiveData<List<Pokemon>>
+        get() = _pokemons
+
+    fun onPokemonSwipe(position: Int) {
+        _pokemons.value = _pokemons.value?.filterIndexed { index, _ -> index != position }
+        Log.e("AAA", _pokemons.value?.map { p -> p.name }.toString())
+    }
+
+    init {
+        _pokemons.value = arrayListOf(
+            Pokemon("Jolteon", "If it is angered or startled, the fur all over its body bristles like sharp needles that pierce foes. ", PokeCategory.Lightning),
+            Pokemon("Bulbasaur", "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.", PokeCategory.Seed),
+            Pokemon("Pikachu", "Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.", PokeCategory.Mouse),
+            Pokemon("Charizard", "It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames. ", PokeCategory.Flame),
+            Pokemon("Raichu", "Its long tail serves as a ground to protect itself from its own high-voltage power. ", PokeCategory.Mouse),
+            Pokemon("Flareon", "Once it has stored up enough heat, this Pokémon’s body temperature can reach up to 1,700 degrees Fahrenheit. ", PokeCategory.Flame),
+            Pokemon("Electrike", "It stores static electricity in its fur for discharging. It gives off sparks if a storm approaches. ", PokeCategory.Lightning),
+            Pokemon("Sunkern", "Sunkern tries to move as little as it possibly can. It does so because it tries to conserve all the nutrients it has stored in its body for its evolution. It will not eat a thing, subsisting only on morning dew. ", PokeCategory.Seed),
+        )
+
+    }
+}
