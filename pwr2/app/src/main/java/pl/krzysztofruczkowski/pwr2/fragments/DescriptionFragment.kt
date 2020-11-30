75536ed128c9164a4f741a1b123669d9eac3811d
tojatos <tojatos@gmail.com>
2020-12-01 00:05:43 +0100

    DescriptionFragment finished

--- pwr2/app/src/main/java/pl/krzysztofruczkowski/pwr2/fragments/DescriptionFragment.kt
+++ pwr2/app/src/main/java/pl/krzysztofruczkowski/pwr2/fragments/DescriptionFragment.kt
@@ -9,17 +9,23 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.fragment_description.*
 import pl.krzysztofruczkowski.pwr2.DetailsActivity
+import pl.krzysztofruczkowski.pwr2.MainActivity
 import pl.krzysztofruczkowski.pwr2.MainViewModel
 import pl.krzysztofruczkowski.pwr2.R
 import pl.krzysztofruczkowski.pwr2.databinding.FragmentDescriptionBinding
+import java.util.*
 
 class DescriptionFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
         val binding = DataBindingUtil.inflate<FragmentDescriptionBinding>(inflater, R.layout.fragment_description, container, false)
+        val pokemon = (activity as DetailsActivity).pokemon
 
         binding.apply {
-            description.text = (activity as DetailsActivity).pokemon.description
+            pokemonName.text = pokemon.name
+            val imageId = MainActivity.app_resources.getIdentifier(pokemon.name.toLowerCase( Locale.ROOT), "drawable", MainActivity.app_package_name)
+            pokemonImage.setBackgroundResource(imageId)
+            description.text = pokemon.description
         }
 
         return binding.root
