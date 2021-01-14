20f7b468d06a105e43afca27f2db01431023f4d4
tojatos <tojatos@gmail.com>
2021-01-14 17:43:25 +0100

    Added directory chooser icon

--- pwr4/app/src/main/java/pl/krzysztofruczkowski/pwr4/MainActivity.kt
+++ pwr4/app/src/main/java/pl/krzysztofruczkowski/pwr4/MainActivity.kt
@@ -6,19 +6,35 @@ import android.os.Bundle
 import android.provider.DocumentsContract
 import android.provider.MediaStore
 import android.util.Log
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.documentfile.provider.DocumentFile
-
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         setContentView(R.layout.activity_main)
-//        val i = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
-//        i.addCategory(Intent.CATEGORY_DEFAULT)
-//        startActivityForResult(Intent.createChooser(i, "Choose directory"), 9999)
+
+        fun startDirectoryChooser() {
+            val i = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+            i.addCategory(Intent.CATEGORY_DEFAULT)
+            startActivityForResult(Intent.createChooser(i, "Choose directory"), 9999)
+        }
+
+        val topAppBar: MaterialToolbar = findViewById(R.id.topAppBar)
+        topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_directory -> {
+                    startDirectoryChooser()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -29,35 +45,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showSongs(treeUri: Uri) {
-//        val uri = DocumentsContract.buildDocumentUriUsingTree(treeUri, DocumentsContract.getDocumentId(treeUri))
         val dir = DocumentFile.fromTreeUri(applicationContext, treeUri)
         if (dir == null || !dir.isDirectory) return
         val names = dir.listFiles().map { f -> f.name }
         Log.i("X", names.toString())
-
-
-//        val selection = MediaStore.Audio.Media.IS_MUSIC + " != 0"
-//
-//        val projection = arrayOf(
-//                MediaStore.Audio.Media._ID,
-//                MediaStore.Audio.Media.ARTIST,
-//                MediaStore.Audio.Media.TITLE,
-//                MediaStore.Audio.Media.DATA,
-//                MediaStore.Audio.Media.DISPLAY_NAME,
-//                MediaStore.Audio.Media.DURATION
-//        )
-//
-//        val cursor = managedQuery(
-//                uri,
-//                projection,
-//                selection,
-//                null,
-//                null)
-//
-//        val songs: MutableList<String> = ArrayList()
-//        while (cursor.moveToNext()) {
-//            songs.add(cursor.getString(0).toString() + "||" + cursor.getString(1) + "||" + cursor.getString(2) + "||" + cursor.getString(3) + "||" + cursor.getString(4) + "||" + cursor.getString(5))
-//        }
-//        Log.i("Test", songs.toString())
     }
 }
